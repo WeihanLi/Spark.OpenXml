a2f937f345fbb39977b2dbf3b4dacb7c2bdae95e
Weihan Li <weihanli@outlook.com>
2026-05-16 14:44:11 +0800

    remove unnecessary polyfill

--- src/Spark.OpenXml/Compat.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Weihan Li. All rights reserved.
-// Licensed under the Apache license.
-#if NETSTANDARD
-// ReSharper disable once CheckNamespace
-namespace System.Runtime.CompilerServices;
-
-internal sealed class IsExternalInit;
-#endif
